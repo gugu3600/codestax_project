a344b4e38f9ecc358a8fbc05e149411ebb6c32b5
gugu36 <jukonzai@gmail.com>
2026-05-12 14:48:24 +0630

    Fixed bugs and added docker

--- frontend/src/routes/routeguard.js
+++ frontend/src/routes/routeguard.js
@@ -3,70 +3,43 @@ import { auth, isLoggedIn, removeAuth, setAuth } from "@/stores/authStore";
 import Cookies from "js-cookie";
 
 export const authGuard = (router) => {
-     router.beforeEach(async (to, from) => {
-          // const token = Cookies.get("token");
-          // console.log(token);
+  router.beforeEach(async (to, from) => {
 
-           if(to.meta.authorize){
-               try {
-                    const res = await authAPI.verify();
-                    setAuth(res);
+    const hasToken = Cookies.get("token");
 
-                    if(res.role_id == 1 || res.role === "admin"){
-                         return true;
-                    }
 
-                    throw new Error("Admin only allowed route");
-               }
+    if (to.meta.authorize) {
+      try {
+        const res = await authAPI.verify();
+        setAuth(res);
+        if (res.role_id == 1 || res.role === "admin") return true;
+        throw new Error("Admin only allowed route");
+      } catch (error) {
+        window.alert(error.message);
+        return "/";
+      }
+    }
 
-               catch(error){
-                    window.alert(error.message);
-                    return ("/");
-               }
-          }
+    if ((to.path === "/login" || to.path === "/register") && hasToken) {
+      try {
+        const res = await authAPI.verify();
+        if (res) return "/";
+      } catch (err) {
+        return true; 
+      }
+    }
+    if (to.path === "/") {
+      if (hasToken && !isLoggedIn.value) {
+        try {
+          const res = await authAPI.verify();
+          setAuth(res);
+        } catch (e) {
+          removeAuth();
+        }
+      }
+      return true;
+    }
 
-          if (to.meta.auth && !isLoggedIn.value) {
-               try {
-                    const res = await authAPI.verify();
-                    setAuth(res);
-                    // console.log(isLoggedIn.value)
-                    return true;
-               } catch (err) {
-                    removeAuth();
-                    window.alert("Logged In required");
-                    return { path: "/" }; 
-               }
-          }
-
-         
-
-          if (to.path === "/login" || to.path === "/register") {
-               if (isLoggedIn.value) {
-                    return { path: "/" };
-               }
-
-               try {
-                    const res = await authAPI.verify();
-                    if (res) {
-                         setAuth(res);
-                         return { path: "/" }; 
-                    }
-               } catch (err) {
-                    return true;
-               }
-          }
-
-          if(to.path === "/"){
-               
-               try {
-                    const res = await authAPI.verify();
-                    setAuth(res);
-                    return true;
-               }
-               catch (error){
-                    return true;
-               }
-          }
-          return true;
-     });
-}
+    return true;
+  });
+};
